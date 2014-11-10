7a080f3d711eea661e0068ec9afe9a1c9d0ac892
Chris <chisnet@hotmail.com>
2014-11-10 14:29:27 +0000

    Basic generate method shell and desired planet types

--- planet-generator/generator.py
+++ planet-generator/generator.py
@@ -1,2 +1,15 @@
 import noise
 
+from PIL import Image
+
+PLANET_TYPES = [
+    'temperate',
+    'gas',
+    'ice',
+    'lava',
+    'dust',
+]
+
+
+def generate(planet_type, size):
+    image = Image.new('RGB', (size, size))
