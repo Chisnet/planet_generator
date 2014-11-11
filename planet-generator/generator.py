ca7bc118e618c6a9de473d6a8beadf1b56d829e4
Chris <chisnet@hotmail.com>
2014-11-11 14:32:40 +0000

    Experiment in generation of grayscale noise image

--- planet-generator/generator.py
+++ planet-generator/generator.py
@@ -1,24 +1,38 @@
 from noise import snoise2
 from PIL import Image
 
-PLANET_TYPES = [
-    'temperate',
-    'gas',
-    'ice',
-    'lava',
-    'dust',
-]
+PLANET_TYPES = {
+    'temperate': {
+
+    },
+    'gas': {
+        'octaves': 16
+    },
+    'ice': {
+
+    },
+    'lava': {
+
+    },
+    'dust': {
+
+    }
+}
 
 
 def generate(planet_type, size):
     image = Image.new('RGB', (size, size))
 
-    octaves = 1
+    octaves = 16
     freq = 16.0 * octaves
+
+    image_array = []
 
     for y in range(size):
         for x in range(size):
+            noise_value = int(snoise2(x / freq, y / freq, octaves) * 127.0 + 128.0)
+            image_array.append((noise_value, noise_value, noise_value))
 
-            print("%s" % int(snoise2(x / freq, y / freq, octaves) * 127.0 + 128.0))
+    image.putdata(image_array)
 
-            # f.write("%s\n" % int(snoise2(x / freq, y / freq, octaves) * 127.0 + 128.0))
+    image.save('test.jpg')
